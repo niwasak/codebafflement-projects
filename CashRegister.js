3d190603be8b465f09265d2a14b8d635c4f17a0f
niwasak <nono.uys@gmail.com>
2020-05-13 20:06:33 +0000

    test from git pod

--- CashRegister.js
+++ CashRegister.js
@@ -14,4 +14,9 @@ function checkCashRegister(price, cash, cid) {
     var output = {status: null, change: []};
     var change = cash - price;
     
+    if (register.total === change) {
+        output.status = "CLOSED";
+        output.change = cid;
+        return output;
+    }
 }
